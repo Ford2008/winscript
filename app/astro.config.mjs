a12854e40923abcc144cbd857f489ddd0539be1b
Ford2008 <dangdang21cn@163.com>
2026-06-23 18:32:42 +0800

    Add Simplified Chinese and Traditional Chinese（添加简体中文与繁体中文）
    Add Simplified Chinese and Traditional Chinese（添加简体中文与繁体中文）

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   i18n: {
-    locales: ["en", "ru", "tr", "it", "ko", "es", "pt", "fr", "vi", "sv", "de", "hu"],
+    locales: ["en","cn","tc", "ru", "tr", "it", "ko", "es", "pt", "fr", "vi", "sv", "de", "hu"],
     defaultLocale: "en",
   },
 });
